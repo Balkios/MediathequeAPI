a0d4e3174f5c6f086ac63504d0bfb16617f538a1
balkios <balkis.ghazouani@yahoo.fr>
2022-02-25 21:18:17 +0100

    first commit

---
+++ src/test/java/org/formation/mediatheque/MediathequeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.formation.mediatheque;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediathequeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
